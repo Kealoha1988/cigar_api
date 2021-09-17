8dc218e98fb7bb728610c62d935689a403e06b6e
Kealoha1988 <kealohaabe1988@gmail.com>
2021-09-17 17:53:30 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cigar.create(brand: "Monte Cristo", line: "White", country: "Dominican Republic", size: "Robusto", about: "One of the most well know cigars. The go to for celebrations of any kind!")
+Cigar.create(brand: "Romeo y Julietta", line: "Reserva Real", country: "Dominican Republic", size: "Church Hill", about: "Ole faithul. A classic that never lets you down")
